deec76159ba3820dc7f6f9dd5d602454fe7ba8c1
sam <samyang3125c@gmail.com>
2020-02-29 16:14:45 +0800

    optimize 199. Binary Tree Right Side View.

--- src/com/sam3125c/leetcode/tree/BinaryTreeRightSideView.java
+++ src/com/sam3125c/leetcode/tree/BinaryTreeRightSideView.java
@@ -6,12 +6,15 @@ package com.sam3125c.leetcode.tree;
 
 import com.sam3125c.datastructure.binarytree.TreeNode;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
 /**
  * 199. Binary Tree Right Side View
- * Runtime: 2 ms, faster than 14.79% of Java online submissions for Binary Tree Right Side View.
- * Memory Usage: 38.8 MB, less than 5.88% of Java online submissions for Binary Tree Right Side View.
+ * Runtime: 1 ms, faster than 80.92% of Java online submissions for Binary Tree Right Side View.
+ * Memory Usage: 38.4 MB, less than 5.88% of Java online submissions for Binary Tree Right Side View.
  *
  * @author Yang Rong
  * @version 1.0
@@ -21,46 +24,34 @@ import java.util.*;
 public class BinaryTreeRightSideView {
 
     public List<Integer>  rightSideView(TreeNode node) {
+        List<Integer> result = new ArrayList<>();
         if (node == null) {
-            return new ArrayList<>();
+            return result;
         }
-        StrengthenNode sNode = new StrengthenNode(node, 1);
-        Map<Integer, Integer> resultMap = new HashMap<>();
-        // 借助队列结构实现按层遍历。
-        Queue<StrengthenNode> queue = new LinkedList<>();
-        queue.offer(sNode);
+        // 借助队列结构实现。
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(node);
+        int depth = 0;
         // 循环到队列为空。
         while (!queue.isEmpty()) {
-            StrengthenNode curr = queue.poll();
-            // 处理逻辑。
-            resultMap.put(curr.depth, curr.node.val);
-            // 先入队左子树。
-            if (curr.node.left != null) {
-                queue.offer(new StrengthenNode(curr.node.left, curr.depth + 1));
+            int size = queue.size();
+            TreeNode curr = null;
+            // 层内遍历。
+            for (int i = 0; i < size; i++) {
+                curr = queue.poll();
+                // 先入队左子树。
+                if (curr.left != null) {
+                    queue.offer(curr.left);
+                }
+                // 再入队右子树。
+                if (curr.right != null) {
+                    queue.offer(curr.right);
+                }
             }
-            // 再入队右子树。
-            if (curr.node.right != null) {
-                queue.offer(new StrengthenNode(curr.node.right, curr.depth + 1));
-            }
+            // 层与层分界点。
+            result.add(curr.val);
         }
-        return new ArrayList<Integer>(resultMap.values());
+        return result;
     }
 
-    /**
-     * 带深度的增强节点。
-     *
-     * @author Yang Rong
-     * @version 1.0
-     * @email samyang3125c@gmail.com
-     * @create 2020-02-29
-     */
-    class StrengthenNode {
-        int depth;
-        TreeNode node;
-
-        StrengthenNode(TreeNode node, int depth) {
-            this.node = node;
-            this.depth = depth;
-        }
-    }
 }
